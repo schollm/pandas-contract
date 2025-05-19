73536bdcd2aad05158438884096f88e8bfc2ac5f
Micha Scholl <8493022+schollm@users.noreply.github.com>
2025-05-19 10:33:58 +0200

    Revert "WIP: Allow Checks to be functions by renaming Checks.mk_check to Checks.__call__."
    This reverts commit ba0dc21ec9b46c5c74ff3ff4c8e19f7dce62c7a7.

--- src/pandas_contract/checks.py
+++ src/pandas_contract/checks.py
@@ -122,7 +122,7 @@ class same_length_as(Check):  # noqa: N801
         """
         self.args = split_or_list(args)
 
-    def __call__(
+    def mk_check(
         self, fn: Callable, args: tuple[Any, ...], kwargs: dict[str, Any]
     ) -> DataCheckFunctionT:
         """Check the DataFrame and keep the index."""
@@ -323,7 +323,7 @@ class is_(Check):  # noqa: N801
         """
         self.arg = arg or ""
 
-    def __call__(
+    def mk_check(
         self, fn: Callable, args: tuple[Any], kwargs: dict[str, Any]
     ) -> DataCheckFunctionT:
         """Create a check function."""
