68348ae4173e5faf2ffa41fbc22130be0c069c71
Micha <8493022+schollm@users.noreply.github.com>
2025-05-21 12:35:08 +0200

    Modes from env do not crash if value is invalid (#52)
    * Log warning if PANDAS_CONTRACT_MODE_ENV is set to an invalid value and info if it's unset. In either case, default to silent mode.
    
    * Make test_modes_handler_logging side-effect-free by setting up and restoring the logger in a fixture.
    
    * Add test for set_mode with invalid value.

--- src/tests/unit_tests/test_mode.py
+++ src/tests/unit_tests/test_mode.py
@@ -1,20 +1,34 @@
 """Test the mode module."""
 
 from collections.abc import Generator
+from typing import TYPE_CHECKING, cast
 
 import pytest
 
-from pandas_contract.mode import Modes, as_mode, get_mode, raises, silent
+from pandas_contract import set_mode
 from pandas_contract.mode import (
-    logger as mode_logger,
+    PANDAS_CONTRACT_MODE_ENV,
+    Modes,
+    _get_mode_from_env,
+    as_mode,
+    get_mode,
+    raises,
+    silent,
 )
+from pandas_contract.mode import logger as mode_logger
+
+if TYPE_CHECKING:
+    from pandas_contract.mode import ModesT
 
 
 @pytest.fixture(autouse=True)
 def set_default_mode() -> Generator[None, None, None]:
     """Set the default mode to raise."""
+    level = mode_logger.getEffectiveLevel()
+    mode_logger.setLevel(-10)
     with as_mode("raise"):
         yield
+    mode_logger.setLevel(level)
 
 
 @pytest.mark.parametrize(
@@ -22,7 +36,6 @@ def set_default_mode() -> Generator[None, None, None]:
 )
 def test_modes_handler_logging(caplog: pytest.LogCaptureFixture, mode: Modes) -> None:
     """Test that the mode handler logs the messages."""
-    mode_logger.setLevel(-10)
     mode.handle(["test-msg"], "prefix: ")
     assert "prefix: test-msg" in caplog.text
 
@@ -97,3 +110,40 @@ def test_mode_logging(mode: Modes, caplog: pytest.LogCaptureFixture) -> None:
     """Test SILENT handling."""
     mode.handle(["err"], "prefix: ")
     assert "prefix: err" in caplog.text
+
+
+def test_set_mode_invalid() -> None:
+    """Test that an invalid mode raises a ValueError."""
+    with pytest.raises(ValueError, match="invalid"):
+        set_mode(cast("ModesT", "invalid"))
+
+
+@pytest.mark.parametrize(
+    "env, expected, log_msg",
+    [
+        ("silent", Modes.SILENT, ""),
+        ("error", Modes.ERROR, ""),
+        (
+            "invalid",
+            Modes.SILENT,
+            "Environment variable PANDAS_CONTRACT_MODE contains invalid value. Setting"
+            " to default mode: silent",
+        ),
+        (
+            "",
+            Modes.SILENT,
+            "No environment variable PANDAS_CONTRACT_MODE set. Default to silent.",
+        ),
+    ],
+)
+def test_mode_from_env(
+    env: str,
+    expected: Modes,
+    log_msg: str,
+    monkeypatch: pytest.MonkeyPatch,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Test _get_mode_from_env."""
+    monkeypatch.setenv(PANDAS_CONTRACT_MODE_ENV, env)
+    assert _get_mode_from_env() == expected
+    assert log_msg in caplog.text
